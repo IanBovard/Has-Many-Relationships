3f4473ffb5f1b493c8da51129766f8d3f1622946
Ian Bovard <thebestpantsintown@gmail.com>
2017-07-24 16:37:32 -1000

    tables

---
+++ joins.sql
@@ -0,0 +1,15 @@
+\c radlad;
+
+DROP DATABASE IF EXISTS has_many_blogs;
+DROP USER IF EXISTS has_many_user;
+
+CREATE USER has_many_user;
+CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
+
+\c has_many_blogs;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS comments;
+
+\i has_many_blogs.sql;
